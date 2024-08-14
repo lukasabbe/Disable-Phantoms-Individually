ffd7c6100c27426891e868b509b8c2d7529a41a2
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-08-14 15:50:32 +0200

    publish workflow

--- src/main/java/me/lukasabbe/disablephantomsindividually/mixin/PhantomSpawnMixin.java
+++ src/main/java/me/lukasabbe/disablephantomsindividually/mixin/PhantomSpawnMixin.java
@@ -1,16 +1,17 @@
 package me.lukasabbe.disablephantomsindividually.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.lukasabbe.disablephantomsindividually.playerdata.InsomniaData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PhantomSpawner.class)
 public class PhantomSpawnMixin {
-    @Redirect(method = "spawn", at= @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isSpectator()Z"))
-    public boolean isSpawningForPlayer(ServerPlayerEntity instance){
-        return instance.isSpectator() || !InsomniaData.getPlayerState(instance).insomniaToggle;
+    @ModifyExpressionValue(method = "spawn", at= @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isSpectator()Z"))
+    public boolean isSpawningForPlayer(boolean original, @Local ServerPlayerEntity player){
+        return original || !InsomniaData.getPlayerState(player).insomniaToggle;
     }
 }
